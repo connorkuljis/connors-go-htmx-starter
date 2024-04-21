64580d4bd8648a4c6eb18b6a4bc33b1867ea373a
connorkuljis <conkuljis@gmail.com>
2024-04-21 15:39:02 +0800

    counter demo

--- pkg/server/handlers.go
+++ pkg/server/handlers.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"fmt"
 	"io/fs"
 	"net/http"
+	"strconv"
 )
 
 // Routes instatiates http Handlers and associated patterns on the server.
@@ -14,6 +16,7 @@ func (s *Server) Routes() error {
 
 	s.MuxRouter.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(scfs))))
 	s.MuxRouter.HandleFunc("/", s.HandleIndex())
+	s.MuxRouter.HandleFunc("/counter", s.HandleCounter())
 
 	return nil
 }
@@ -31,8 +34,57 @@ func (s *Server) HandleIndex() http.HandlerFunc {
 
 	indexTemplate := s.BuildTemplates("index", nil, indexTemplateFragments...)
 
+	data := map[string]any{
+		"Count": 0,
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		htmlBytes, err := SafeTmplExec(indexTemplate, "root", nil)
+		htmlBytes, err := SafeTmplExec(indexTemplate, "root", data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		SendHTML(w, htmlBytes)
+	}
+}
+
+func (s *Server) HandleCounter() http.HandlerFunc {
+	fragment := s.TemplateFragments.Views["index.html"]
+
+	counterTemplate := s.BuildTemplates("counter", nil, fragment)
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+
+		strCount := r.Form.Get("count")
+		if strCount == "" {
+			http.Error(w, "Error: no form value for 'count'", http.StatusInternalServerError)
+			return
+		}
+
+		count, err := strconv.Atoi(strCount)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		op := r.URL.Query().Get("op")
+		switch op {
+		case "add":
+			count++
+			fmt.Fprintf(w, "Add operation")
+		case "subtract":
+			count--
+			fmt.Fprintf(w, "Subtract operation")
+		default:
+			fmt.Fprintf(w, "Updated count to %d", count)
+		}
+
+		data := map[string]any{
+			"Count": count,
+		}
+
+		htmlBytes, err := SafeTmplExec(counterTemplate, "counter", data)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
